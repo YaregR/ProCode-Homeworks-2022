8ec4ef232dd1ae2c5b390e5246608ee8c8282a9e
Yaroslav Rudenko <yaregr@gmail.com>
2022-06-22 12:42:15 +0300

    HW-18.06.2022-MoveBlock add Level 2

--- HW-18.06.2022-MoveBlock/main.js
+++ HW-18.06.2022-MoveBlock/main.js
@@ -7,28 +7,32 @@ const arrowRight = document.querySelector('.arrow-right');
 
 arrowUp.addEventListener('click', () => {
     let elBlockTop=parseInt(elBlock.style.top);
-    if(elBlockTop > 0 && elBlockTop < 600) {
+    let elBlockLeft=parseInt(elBlock.style.left);
+    if((elBlockTop > 0 && elBlockTop < 600) && (elBlockLeft !== 500 || elBlockTop !== 200)) {
         elBlock.style.top=parseInt(elBlock.style.top)-100 +'px';
     } else return;
 });
 
 arrowDown.addEventListener('click', () => {
     let elBlockTop=parseInt(elBlock.style.top);
-    if(elBlockTop === 0 || elBlockTop < 500) {
+    let elBlockLeft=parseInt(elBlock.style.left);
+    if((elBlockTop === 0 || elBlockTop < 500) && (elBlockLeft !== 500 || elBlockTop !== 0)) {
         elBlock.style.top=parseInt(elBlock.style.top)+100 +'px';
     } else return;
 });
 
 arrowLeft.addEventListener('click', () => {
+    let elBlockTop=parseInt(elBlock.style.top);
     let elBlockLeft=parseInt(elBlock.style.left);
-    if(elBlockLeft > 0 && elBlockLeft < 800) {
+    if((elBlockLeft > 0 && elBlockLeft < 800) && (elBlockLeft !== 600 || elBlockTop !== 100)) {
         elBlock.style.left=parseInt(elBlock.style.left)-100 +'px';
     } else return;
 });
 
 arrowRight.addEventListener('click', () => {
+    let elBlockTop=parseInt(elBlock.style.top);
     let elBlockLeft=parseInt(elBlock.style.left);
-    if(elBlockLeft === 0 || elBlockLeft < 700) {
+    if((elBlockLeft === 0 || elBlockLeft < 700) && (elBlockLeft !== 400 || elBlockTop !== 100)) {
         elBlock.style.left=parseInt(elBlock.style.left)+100 +'px';
     } else return;
 }); 
@@ -38,22 +42,22 @@ addEventListener('keydown', (ev) => {
     let elBlockLeft=parseInt(elBlock.style.left);
     switch (ev.code) {
         case 'ArrowUp':
-            if(elBlockTop > 0 && elBlockTop < 600) {
+            if((elBlockTop > 0 && elBlockTop < 600) && (elBlockLeft !== 500 || elBlockTop !== 200)) {
                 elBlock.style.top=parseInt(elBlock.style.top)-100 +'px';
             } else return;
             break;
         case 'ArrowDown':
-            if(elBlockTop === 0 || elBlockTop < 500) {
+            if((elBlockTop === 0 || elBlockTop < 500) && (elBlockLeft !== 500 || elBlockTop !== 0)) {
                 elBlock.style.top=parseInt(elBlock.style.top)+100 +'px';
             } else return;
             break; 
         case 'ArrowLeft': 
-            if(elBlockLeft > 0 && elBlockLeft < 800) {
+            if((elBlockLeft > 0 && elBlockLeft < 800) && (elBlockLeft !== 600 || elBlockTop !== 100)) {
                 elBlock.style.left=parseInt(elBlock.style.left)-100 +'px';
             } else return;
             break; 
         case 'ArrowRight': 
-            if(elBlockLeft === 0 || elBlockLeft < 700) {
+            if((elBlockLeft === 0 || elBlockLeft < 700) && (elBlockLeft !== 400 || elBlockTop !== 100)) {
                 elBlock.style.left=parseInt(elBlock.style.left)+100 +'px';
             } else return;
             break;    
