6cf10dd56d9d2b45f04e2eda181ce8eeba98f415
Yaroslav Rudenko <yaregr@gmail.com>
2022-08-18 19:14:02 +0300

    add HW-11.08.2022-ejs level3 + preloader

--- HW-11.08.2022-ejs/public/js/main.js
+++ HW-11.08.2022-ejs/public/js/main.js
@@ -1,6 +1,11 @@
-// const axios = require('axios');
+
 const itemEl = document.querySelector('.items-wrapper');
-console.log('test');
+const preloader = document.querySelector('.preloader');
+
+window.onload = function () {
+    preloader.style.display = 'none';
+}
+
 
 const getItems = async() => {
     const items = await axios.get('http://localhost:3000/items');
@@ -8,8 +13,7 @@ const getItems = async() => {
     for (let i in items.data) { 
         let item = `<a href="#"><div class="main-item">` + items.data[i].name + "</div></a>"; 
         itemEl.insertAdjacentHTML('beforeend', item);
-    }
-   
+    }  
 }
 
 getItems();
